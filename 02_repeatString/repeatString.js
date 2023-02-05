c7189ca559a1caf795154f17906c328f4fd68888
luis <114965640+Angelnum18@users.noreply.github.com>
2023-02-05 01:29:55 -0400

    01 Hello, World | 02 repeatString, Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    message = text
+    let i = number;
+    for (i = number;i > 0; --i) {
+        if (i == 1) {
+            message = message;
+            break;
+            } else if (i == 0) {
+            message = "Please enter a number";
+            } else {
+            message += text;
+            }
+    }
+return message
 };
 
 // Do not edit below this line
