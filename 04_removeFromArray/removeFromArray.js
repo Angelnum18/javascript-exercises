9d2db9a542e9b0c76b6c6b81063f4f832d1be943
luis <114965640+Angelnum18@users.noreply.github.com>
2023-02-21 18:07:36 -0400

    04 removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,58 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let resultArray;
+    let a;
+    let b;
+    
+    
 
+        for (let j = args.length -1;j >= 0; j--) {
+
+
+
+        for (let i = array.length - 1;i >= 0; i--) {
+            if (array[i] === args[j]) {
+                a = array.slice(0, i);
+                b = array.slice(i + 1, array.length);
+                array = a.concat(b);
+            } else {
+                continue;
+            }
+        };
+    }
+
+
+
+
+
+/*     for (let i = 0; i <= array.length -1; i++) {
+        if (array[i] = arg1) {
+            if (i = 0) {
+                array.shift();
+            } else if (i = array.length - 1) {
+                array.pop();
+            } else {
+                a = array.slice(0, i + 1);
+                b = array.slice(i, array.length);
+                resultArray = a.concat(b);
+            }
+
+           resultArray = array.slice(i, array.length);
+            break
+
+
+
+        } else {
+            resultArray = resultArray;
+            continue
+        }
+    } */
+/*     console.log(a);
+    console.log(b);
+    console.log(resultArray); */
+
+    /* resultArray = i; */
+    resultArray = array;
+    return resultArray;
 };
 
 // Do not edit below this line
