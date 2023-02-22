ec9f17eff3e3193c9eb9bb5c08729125ceb8ce40
luis <114965640+Angelnum18@users.noreply.github.com>
2023-02-22 14:34:55 -0400

    05 sumALl Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(suma1, suma2) {
+let finalResult = 0;
+let a = 0;
+let b = 0; 
 
+if (typeof(suma1) == 'number' && typeof(suma2) == 'number') {
+        if ((suma1 > 0) && (suma2 > 0)) {
+            if (suma1 > suma2) {
+                for (let i = 1; i <= suma1; i++) {
+                    a += i;
+                };
+                finalResult = a;
+            } else {
+                for (let i = 1; i <= suma2; i++) {
+                    b += i;
+                };
+                finalResult = b;
+            };
+
+
+            
+        } else {
+            finalResult = 'ERROR';
+        }
+    } else {
+        finalResult = 'ERROR';
+    };
+
+    
+return finalResult;
 };
 
 // Do not edit below this line
