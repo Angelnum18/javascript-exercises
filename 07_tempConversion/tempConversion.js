8abee0d7e2f9d505e4e25b478a8eb116ef56f2cd
luis <114965640+Angelnum18@users.noreply.github.com>
2023-02-24 22:57:33 -0400

    07 tempConversion COmpleted

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let f = tempF;
+  let x = 1;
+  let result = 0;
+
+
+  result = Math.round(((f - 32) * (5/ 9) * x) * 10) / 10;
+   /*  f = tempF * 2; */
+
+  console.log(result);
+
+
+return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let c = tempC;
+  let x = 1;
+  let result = 0;
+
+  result = Math.round(((c * (9/ 5) + 32) * x)* 10) / 10;
+
+/*     c = tempC * 3; */
+
+  console.log(result);
+return result
 };
 
 // Do not edit below this line
